6724cc0415174399f5bd29e3f2e2e80905d86c70
GamarraDiego <diegolgamarra@gmail.com>
2025-05-28 19:19:44 -0300

    Tablero funcionando, ponganlo lindo, salu2

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { Button } from "react-native-web";
 
 export default function App() {
@@ -32,7 +32,7 @@ export default function App() {
     Trato de explicar. Básicamente te hacés una copia del tablero y le ponés la nueva jugada, X ó O.
     Luego, te fijás si ese tablero actualizado configura una de las combinaciones para ganar.
     Si lo es, seteás el ganador y a otra cosa mariposa.
-    Sino,  seteás el próximo turno para el otro jugador.
+    Sino, seteás el próximo turno para el otro jugador.
   */
   const eventoPresionar = indice => {
     if (tablero[indice] || ganador) // Si ya hay un cosito en la celda o hay ganador, no se hace nada
@@ -51,22 +51,41 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <View>
-        <Button styles={styles.botoncito}></Button>
-        <Button styles={styles.botoncito}></Button>
-        <Button styles={styles.botoncito}></Button>
-      </View>
-      <View>
-        <Button styles={styles.botoncito}></Button>
-        <Button styles={styles.botoncito}></Button>
-        <Button styles={styles.botoncito}></Button>
-      </View>
-      <View>
-        <Button styles={styles.botoncito}></Button>
-        <Button styles={styles.botoncito}></Button>
-        <Button styles={styles.botoncito}></Button>
+      <Text>Ta te ti</Text>
+      <Text>
+        {ganador ? ganador === 'Empate' ? 'Empate' : `Ganó ${ganador}` : `Turno de ${turnoActual}`}
+      </Text>
+      <View style={styles.tablero}>
+          <TouchableOpacity style={styles.botoncito} onPress={() => eventoPresionar(0)}>
+            <Text style={styles.textoBotoncito}>{tablero[0]}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.botoncito} onPress={() => eventoPresionar(3)}>
+            <Text style={styles.textoBotoncito}>{tablero[3]}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.botoncito} onPress={() => eventoPresionar(6)}>
+            <Text style={styles.textoBotoncito}>{tablero[6]}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.botoncito} onPress={() => eventoPresionar(1)}>
+            <Text style={styles.textoBotoncito}>{tablero[1]}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.botoncito} onPress={() => eventoPresionar(4)}>
+            <Text style={styles.textoBotoncito}>{tablero[4]}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.botoncito} onPress={() => eventoPresionar(7)}>
+            <Text style={styles.textoBotoncito}>{tablero[7]}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.botoncito} onPress={() => eventoPresionar(2)}>
+            <Text style={styles.textoBotoncito}>{tablero[2]}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.botoncito} onPress={() => eventoPresionar(5)}>
+            <Text style={styles.textoBotoncito}>{tablero[5]}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.botoncito} onPress={() => eventoPresionar(8)}>
+            <Text style={styles.textoBotoncito}>{tablero[8]}</Text>
+          </TouchableOpacity>
       </View>
     </View>
+    
   );
 }
 
@@ -76,15 +95,26 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    flexDirection: "row",
-    flexWrap: "wrap",
+    padding: 20,
+  },
+  tablero: {
+    width: 300,
+    height: 300,
+    flexDirection: 'row',
+    flexWrap: 'wrap'
   },
   botoncito: {
-    borderColor: "black",
-    color: "white",
-    padding: 20,
-    width: 200,
-    height: 200,
+    width: '33.33%',
+    height: '33.33%',
+    borderWidth: 1,
+    borderColor: '#000',
+    backgroundColor: '#2196F3',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textoBotoncito: {
+    fontSize: 48,
+    color: 'black'
   },
   filas: {
     flexDirection: "row",
