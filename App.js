38fab2f1b48867c081ce733b315e24c8a6b734a1
GamarraDiego <diegolgamarra@gmail.com>
2025-05-28 18:09:11 -0300

    tablerito como estado y funcion para verificar

--- App.js
+++ App.js
@@ -1,8 +1,33 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-web";
 
 export default function App() {
+  const [tablero, setTablero] = useState(Array(9).fill(null));
+  const [turnoActual, setTurnoActual] = useState('X');
+  const [ganador, setGanador] = useState(null);
+
+  const combinaciones = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const verificarSiGano = tablero => {
+    for (let [a, b, c] of combinaciones) {
+      if (tablero[a] && tablero[a] === tablero[b] && tablero[a] === tablero[c]) {
+        return tablero[a];
+      }
+    }
+    return tablero.every(celda => celda) ? 'Empate' : null;
+  };
+
   return (
     <View style={styles.container}>
       <View>
